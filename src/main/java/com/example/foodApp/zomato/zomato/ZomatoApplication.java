41db256717b360aee6c444fa67b3703cdb659ecd
AnshulBharadwaj73 <anshulasaperson@gmail.com>
2024-10-09 02:13:57 +0100

    added env variable

--- src/main/java/com/example/foodApp/zomato/zomato/ZomatoApplication.java
+++ src/main/java/com/example/foodApp/zomato/zomato/ZomatoApplication.java
@@ -2,8 +2,12 @@ package com.example.foodApp.zomato.zomato;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.foodApp.zomato.zomato.repositories")
+@EntityScan(basePackages = "com.example.foodApp.zomato.zomato.entities")
 public class ZomatoApplication {
 
 	public static void main(String[] args) {
